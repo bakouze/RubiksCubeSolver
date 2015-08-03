d1d25194d8739295aff873273436279aa16e7805
bakouze <boulanger.guillaume21@gmail.com>
2015-08-03 13:15:01 +0200

    Add attribut and fisrt construtor to the RubiksCube class

--- RubiksCubeSolver/src/RubiksCubeSolver/RubiksCube.java
+++ RubiksCubeSolver/src/RubiksCubeSolver/RubiksCube.java
@@ -1,5 +1,32 @@
 package RubiksCubeSolver;
 
-public class RubiksCube {
+/**
+ * Main Class of the project, construction of the rubiks cube and modelisation of the mouvements.
+ * @author bakouze
+ *
+ */
 
+public class RubiksCube {
+	
+	/**
+	 * The table containing the value of the colors on the faces of the cube
+	 */
+	private String[] cube;
+	
+	/**
+	 * Constructor :
+	 * Built a solved rubiks cube
+	 */
+	public RubiksCube(){
+		String[] c = {
+				"W","W","W","W","W","W","W","W","W",
+				"R","R","R","R","R","R","R","R","R",
+				"B","B","B","B","B","B","B","B","B",
+				"G","G","G","G","G","G","G","G","G",
+				"O","O","O","O","O","O","O","O","O",
+				};
+		this.cube = c;
+	}
+	
+	
 }
