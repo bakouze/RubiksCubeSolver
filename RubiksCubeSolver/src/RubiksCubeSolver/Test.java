400264db5f249023aac18aae635219606ddc49c1
bakouze <boulanger.guillaume21@gmail.com>
2015-08-05 17:09:15 +0200

    Add Test Class to test functions of other classes

---
+++ RubiksCubeSolver/src/RubiksCubeSolver/Test.java
@@ -0,0 +1,19 @@
+package RubiksCubeSolver;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	/**
+	 * Test the property "4*mouv = Id" to test the mouv function
+	 */
+	public static void test1(){
+		RubiksCube cube = new RubiksCube();
+		System.out.println(cube.toString());
+		cube.move(4, "Y");
+		System.out.println(cube.toString());
+	}
+
+}
